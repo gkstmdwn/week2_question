ba74b912d3ed2a68217e273d1dbe4038305e484b
SeungJoo Han <sugarcanes0118@gmail.com>
2025-03-17 17:09:30 +0900

    Update priority_queue.py

--- priority_queue.py
+++ priority_queue.py
@@ -10,8 +10,7 @@ class PriorityQueue:
   def pop(self):
     if len(self.heap) > 1:
       self._swap(0, len(self.heap) - 1)
-      priority, item = self.heap.pop()
-      del priority
+      _, item = self.heap.pop()
       self._sift_down(0)
       return item
     elif len(self.heap) == 1:
